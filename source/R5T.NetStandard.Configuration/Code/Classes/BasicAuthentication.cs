193571f5779bb847a461ffd616c353421c20c686
MinexAutomation <minex.automation@gmail.com>
2019-09-08 16:38:18 -0700

    Added BasicAuthentication functionality. Might be better in a security-focused library.

---
+++ source/R5T.NetStandard.Configuration/Code/Classes/BasicAuthentication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.Configuration
+{
+    public class BasicAuthentication
+    {
+        public string UserID { get; set; }
+        public string Password { get; set; }
+    }
+}
